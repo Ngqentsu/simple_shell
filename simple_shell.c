a41eebd3f8f3f9d1567f7b388f8c7de1d8a3ed85
Simbonge Ngqentsu <ngqentsu0307@gmail.com>
2023-05-23 16:45:03 -0700

    UNIX command line interpreter

---
+++ simple_shell.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+int main(void)
+{
+char *cmd = NULL, *cmd_cp = NULL, *token = NULL, *delim = " \n";
+size_t n = 0;
+int i = 0, argc = 0;
+char **argv = NULL;
+pid_t pid;
+
+while(1)
+{
+printf("$ ");
+
+if(getline(&cmd, &n, stdin) == -1)
+{
+printf("Exiting...\n");
+break;
+}
+
+cmd_cp = strdup(cmd);
+i = 0, argc = 0;
+token = strtok(cmd, delim);
+
+while(token != NULL)
+{
+token = strtok(NULL, delim);
+argc++;
+}
+
+argv = malloc(sizeof(char *) * argc);
+token = strtok(cmd_cp, delim);
+
+while(token != NULL)
+{
+argv[i] = token;
+token = strtok(NULL, delim);
+i++;
+}
+argv[i] = NULL;
+
+for (i = 0; argv[i] != NULL; i++)
+{
+printf("%s\n", argv[i]);
+}
+
+if (strcmp(argv[0], "exit") == 0)
+{
+printf("Exiting...\n");
+break;
+}
+
+pid = fork();
+if (pid == -1)
+return (-1);
+
+if (pid == 0)
+{
+int check = execve(argv[0], argv, NULL);
+if (check == -1)
+perror("Error");
+}
+else
+{
+wait(NULL);
+}
+}
+
+free(cmd), free(cmd_cp), free(argv);
+
+return (0);
+}
