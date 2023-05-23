5ca5bae32f69a06965571d2d0c720d92f3b23038
Simbonge Ngqentsu <ngqentsu0307@gmail.com>
2023-05-23 16:58:28 -0700

    betty code

---
+++ bettycode.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - diving two numbers
+ *
+ * Return: quotient of the two numbers
+ */
+int main(void)
+{
+int a, b;
+int quotient = a / b;
+
+return (c >= 'a' && c <= 'z');
+}
